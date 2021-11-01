0087b634f92945c23f7c3c42db87b05d449f2263
joedietrich-dev <joe@joedietrich.net>
2021-10-31 21:38:13 -0400

    commit tree 6de814764e9ded6bd0caa527455f6f32b34d6397

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  result = ""
+  case array.length
+  when 0 then result = ""
+  when 1 then result = array[0]
+  when 2 then result = "#{array[0]} and #{array[1]}"
+  else 
+    result << array.shift
+    last_word = ", and " + array.pop
+    array.each do |word|
+      result << ", " + word
+    end
+    result << last_word
+  end
+  result
 end
